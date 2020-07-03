2430d1745a5f35f4843428756e0c61f46b6856ba
Le7o <7demijolla@gmail.com>
2020-07-03 15:07:26 +0200

    XML configuration

---
+++ src/main/java/fr/le7o/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,13 @@
+package fr.le7o.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class ChuckConfiguration {
+    //@Bean
+   // public ChuckNorrisQuotes chuckNorrisQuotes(){
+     //   return new ChuckNorrisQuotes();
+    //}
+}
